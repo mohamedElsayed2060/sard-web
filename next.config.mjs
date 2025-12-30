c179630369f9a306dc7aadc7dd0af98b93170299
mohamedElsayed2060 <mohamed.elsayed3240@gmail.com.com>
2025-12-31 01:26:28 +0200

    fix images issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
+      // ✅ local CMS (لو بتحتاجه)
       {
         protocol: "http",
         hostname: "localhost",
         port: "3000",
-        pathname: "/api/media/file/**",
+        pathname: "/**",
+      },
+
+      // ✅ Railway CMS
+      {
+        protocol: "https",
+        hostname: "sard-cms-production.up.railway.app",
+        pathname: "/**",
       },
     ],
   },
